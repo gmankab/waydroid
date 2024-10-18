2c10b4847ead34ee00dea52c6767b4b744f3ecbf
gmanka <gmankab@gmail.com>
2024-10-18 04:09:28 +0400

    [-] cleanup

--- scripts/waydroid-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo test
-sudo waydroid --details-to-stdout init --system_channel=https://ota.waydro.id/system --vendor_channel=https://ota.waydro.id/vendor
-sudo waydroid --details-to-stdout container start
-
